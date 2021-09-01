2316848761de215bde21c664e505724b27293eb6
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2021-09-01 13:03:45 +0530

    added product list page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,94 @@ import { Link } from "react-router-dom";
 import "../assets/stylesheets/Common.css";
 import "../assets/stylesheets/Home.css";
 
+import icon from "../assets/image.png";
+
 export default function Home() {
+  function onClickProduct() {
+    console.log("taking you to the product page");
+  }
+
   return (
-    <div className="container">
-      <Link to={"/login"} className="loginButton">
-        Log In
-      </Link>
+    <div className="container" style={{ maxWidth: "1000px" }}>
+      <div className="headerBar">
+        <div className="titleBar">
+          <div className="titleText">Shopping</div>
+          <Link to={"/login"} className="loginButton">
+            Log In
+          </Link>
+        </div>
+        <input
+          type="text"
+          className="textInput"
+          id="searchBox"
+          placeholder="Search for products"
+        />
+      </div>
+      <div className="wrapper">
+        <div className="product" onClick={onClickProduct}>
+          <img src={icon} className="productImage" alt="" />
+          <div className="productDetails">
+            <div className="name">Product Name</div>
+            <div className="price">₹ 200</div>
+          </div>
+        </div>
+        <div className="product" onClick={onClickProduct}>
+          <img src={icon} className="productImage" alt="" />
+          <div className="productDetails">
+            <div className="name">Product Name</div>
+            <div className="price">₹ 200</div>
+          </div>
+        </div>
+        <div className="product" onClick={onClickProduct}>
+          <img src={icon} className="productImage" alt="" />
+          <div className="productDetails">
+            <div className="name">Product Name</div>
+            <div className="price">₹ 200</div>
+          </div>
+        </div>
+        <div className="product" onClick={onClickProduct}>
+          <img src={icon} className="productImage" alt="" />
+          <div className="productDetails">
+            <div className="name">Product Name</div>
+            <div className="price">₹ 200</div>
+          </div>
+        </div>
+        <div className="product" onClick={onClickProduct}>
+          <img src={icon} className="productImage" alt="" />
+          <div className="productDetails">
+            <div className="name">Product Name</div>
+            <div className="price">₹ 200</div>
+          </div>
+        </div>
+        <div className="product" onClick={onClickProduct}>
+          <img src={icon} className="productImage" alt="" />
+          <div className="productDetails">
+            <div className="name">Product Name</div>
+            <div className="price">₹ 200</div>
+          </div>
+        </div>
+        <div className="product" onClick={onClickProduct}>
+          <img src={icon} className="productImage" alt="" />
+          <div className="productDetails">
+            <div className="name">Product Name</div>
+            <div className="price">₹ 200</div>
+          </div>
+        </div>
+        <div className="product" onClick={onClickProduct}>
+          <img src={icon} className="productImage" alt="" />
+          <div className="productDetails">
+            <div className="name">Product Name</div>
+            <div className="price">₹ 200</div>
+          </div>
+        </div>
+        <div className="product" onClick={onClickProduct}>
+          <img src={icon} className="productImage" alt="" />
+          <div className="productDetails">
+            <div className="name">Product Name</div>
+            <div className="price">₹ 200</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
