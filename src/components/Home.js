04ec1a72cd43fe541b879ea90d6499df774396ea
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2021-09-01 18:52:43 +0530

    fetch products from DB

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,46 @@
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 import "../assets/stylesheets/Common.css";
 import "../assets/stylesheets/Home.css";
 
 import icon from "../assets/image.png";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  function onClickProduct() {
-    console.log("taking you to the product page");
+  /* useState for product list */
+  const [products, setProducts] = useState([]);
+
+  /* Template for each product card */
+  const ProductCard = ({ onClickProduct, icon, productInfo }) => (
+    <div className="product" onClick={() => onClickProduct(productInfo._id)}>
+      <img src={icon} className="productImage" alt="" />
+      <div className="productDetails">
+        <div className="name">{productInfo.title}</div>
+        <div className="price">₹{productInfo.price}</div>
+      </div>
+    </div>
+  );
+
+  function onClickProduct(productId) {
+    console.log(`taking you to the product page for ${productId}`);
   }
+
+  useEffect(() => {
+    /* Fetch 40 products by default */
+    axios
+      .get("https://ccch1.sse.codesandbox.io/products/40")
+      .then((res) => {
+        console.log(res.data);
+
+        setProducts(res.data);
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err.response.data);
+        }
+      });
+  }, []);
 
   return (
     <div className="container" style={{ maxWidth: "1000px" }}>
@@ -27,69 +59,13 @@ export default function Home() {
         />
       </div>
       <div className="wrapper">
-        <div className="product" onClick={onClickProduct}>
-          <img src={icon} className="productImage" alt="" />
-          <div className="productDetails">
-            <div className="name">Product Name</div>
-            <div className="price">₹ 200</div>
-          </div>
-        </div>
-        <div className="product" onClick={onClickProduct}>
-          <img src={icon} className="productImage" alt="" />
-          <div className="productDetails">
-            <div className="name">Product Name</div>
-            <div className="price">₹ 200</div>
-          </div>
-        </div>
-        <div className="product" onClick={onClickProduct}>
-          <img src={icon} className="productImage" alt="" />
-          <div className="productDetails">
-            <div className="name">Product Name</div>
-            <div className="price">₹ 200</div>
-          </div>
-        </div>
-        <div className="product" onClick={onClickProduct}>
-          <img src={icon} className="productImage" alt="" />
-          <div className="productDetails">
-            <div className="name">Product Name</div>
-            <div className="price">₹ 200</div>
-          </div>
-        </div>
-        <div className="product" onClick={onClickProduct}>
-          <img src={icon} className="productImage" alt="" />
-          <div className="productDetails">
-            <div className="name">Product Name</div>
-            <div className="price">₹ 200</div>
-          </div>
-        </div>
-        <div className="product" onClick={onClickProduct}>
-          <img src={icon} className="productImage" alt="" />
-          <div className="productDetails">
-            <div className="name">Product Name</div>
-            <div className="price">₹ 200</div>
-          </div>
-        </div>
-        <div className="product" onClick={onClickProduct}>
-          <img src={icon} className="productImage" alt="" />
-          <div className="productDetails">
-            <div className="name">Product Name</div>
-            <div className="price">₹ 200</div>
-          </div>
-        </div>
-        <div className="product" onClick={onClickProduct}>
-          <img src={icon} className="productImage" alt="" />
-          <div className="productDetails">
-            <div className="name">Product Name</div>
-            <div className="price">₹ 200</div>
-          </div>
-        </div>
-        <div className="product" onClick={onClickProduct}>
-          <img src={icon} className="productImage" alt="" />
-          <div className="productDetails">
-            <div className="name">Product Name</div>
-            <div className="price">₹ 200</div>
-          </div>
-        </div>
+        {products.map((product) => (
+          <ProductCard
+            onClickProduct={onClickProduct}
+            icon={icon}
+            productInfo={product}
+          />
+        ))}
       </div>
     </div>
   );
